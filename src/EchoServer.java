cc2113353226f0e1005384905a0e54162dd61073
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-06-13 14:36:57 +0600

    Added Echoserver class that will get and send message

---
+++ src/EchoServer.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EchoServer {
+    private final int port;
+    private final ExecutorService pool = Executors.newCachedThreadPool();
+    
+    private EchoServer(int port) {
+        this.port = port;
+
+    }
+
+    public static EchoServer bindToPort(int port) {
+        return new EchoServer(port);
+    }
+
+    public void run() {
+        System.out.println("Connecting..........");
+        try (ServerSocket server = new ServerSocket(port)) {
+            while (!server.isClosed()) {
+                Socket clientSocket = server.accept();
+                new UsersList();
+                UsersList.addUser(clientSocket);
+                pool.submit(() -> ClientAccept.run(clientSocket));
+            }
+        } catch (IOException e) {
+            System.out.printf("Probably the port %s is busy!", port);
+            e.printStackTrace();
+        }
+
+        System.out.println("Hello");
+    }
+
+}
